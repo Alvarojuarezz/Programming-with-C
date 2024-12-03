c16198751bc601afb276c39f259cdc03de1ee60c
Álvaro Juárez García <ajuarez0@alu.ucam.edu>
2024-12-03 16:35:44 +0100

    Delete Program directory

--- Program/funciones.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-
-#include "definiciones.h"
-
-struct equipo *insertar_equipo(struct equipo *equipos);
-struct pruebaCiclista *insertar_prueba(struct pruebaCiclista *pruebas);
-
-#endif // FUNCIONES_H_INCLUDED
